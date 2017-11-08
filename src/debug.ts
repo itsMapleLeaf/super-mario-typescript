785cfd157504291f640b904c686b650a425dd1da
kingdaro <kingdaro@gmail.com>
2017-11-08 08:48:53 -0500

    fix errors under strictFunctionTypes

--- src/debug.ts
+++ src/debug.ts
@@ -3,31 +3,26 @@ import { Entity } from './Entity'
 
 const mouseEvents = ['mousedown', 'mousemove']
 
-export function setupMouseControl(
-  canvas: HTMLCanvasElement,
-  entity: Entity,
-  camera: Camera,
-) {
-  let lastEvent: MouseEvent | undefined
+export function setupMouseControl(canvas: HTMLCanvasElement, entity: Entity, camera: Camera) {
+  let lastEvent: MouseEvent | void
 
   mouseEvents.forEach(eventName => {
-    canvas.addEventListener(eventName, (event: MouseEvent) => {
-      if (event.buttons === 1) {
-        entity.vel.set(0, 0)
-        entity.pos.set(
-          event.offsetX + camera.pos.x,
-          event.offsetY + camera.pos.y,
-        )
-      } else if (
-        event.buttons === 2 &&
-        lastEvent &&
-        lastEvent.buttons === 2 &&
-        lastEvent.type === 'mousemove'
-      ) {
-        camera.pos.x -= event.offsetX - lastEvent.offsetX
-      }
+    canvas.addEventListener(eventName, event => {
+      if (event instanceof MouseEvent) {
+        if (event.buttons === 1) {
+          entity.vel.set(0, 0)
+          entity.pos.set(event.offsetX + camera.pos.x, event.offsetY + camera.pos.y)
+        } else if (
+          event.buttons === 2 &&
+          lastEvent &&
+          lastEvent.buttons === 2 &&
+          lastEvent.type === 'mousemove'
+        ) {
+          camera.pos.x -= event.offsetX - lastEvent.offsetX
+        }
 
-      lastEvent = event
+        lastEvent = event
+      }
     })
   })
 
