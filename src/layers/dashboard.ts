dce62dcca488de3146e9501f7c6473c5b8d8b326
kingdaro <kingdaro@gmail.com>
2020-05-05 21:55:00 -0500

    fix crash on death

--- src/layers/dashboard.ts
+++ src/layers/dashboard.ts
@@ -9,7 +9,6 @@ function getPlayerTrait(level: Level) {
     const trait = entity.getTrait(Player)
     if (trait) return trait
   }
-  throw new Error('player not found')
 }
 
 function getTimerTrait(level: Level) {
@@ -17,37 +16,38 @@ function getTimerTrait(level: Level) {
     const trait = entity.getTrait(LevelTimer)
     if (trait) return trait
   }
-  throw new Error('timer not found')
 }
 
 export function createDashboardLayer(font: Font, level: Level) {
   const line1 = font.size
   const line2 = font.size * 2
 
-  const timerTrait = getTimerTrait(level)
-
   return function drawDashboard(context: CanvasRenderingContext2D) {
-    const playerTrait = getPlayerTrait(level)
+    const player = getPlayerTrait(level)
+    if (player) {
+      font.print(player.name, context, 16, line1)
+      font.print(String(player.score).padStart(6, '0'), context, 16, line2)
 
-    font.print(playerTrait.name, context, 16, line1)
-    font.print(String(playerTrait.score).padStart(6, '0'), context, 16, line2)
-
-    font.print(
-      '@x' + String(playerTrait.coins).padStart(2, '0'),
-      context,
-      96,
-      line2,
-    )
+      font.print(
+        '@x' + String(player.coins).padStart(2, '0'),
+        context,
+        96,
+        line2,
+      )
+    }
 
     font.print(`WORLD`, context, 152, line1)
     font.print(level.name, context, 160, line2)
 
-    font.print('TIME', context, 208, line1)
-    font.print(
-      String(Math.floor(timerTrait.currentTime)).padStart(3, '0'),
-      context,
-      216,
-      line2,
-    )
+    const timer = getTimerTrait(level)
+    if (timer) {
+      font.print('TIME', context, 208, line1)
+      font.print(
+        String(Math.floor(timer.currentTime)).padStart(3, '0'),
+        context,
+        216,
+        line2,
+      )
+    }
   }
 }
